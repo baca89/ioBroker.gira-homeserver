5bc078621cb21ceaf556db3cbc0e17fa03269344
Carsten Bauer <home-system@magenta.de>
2023-04-10 21:02:56 +0000

    implements some tests for gira Homewerver types

--- test/homeserverTypes.test.js
+++ test/homeserverTypes.test.js
@@ -1,24 +1,106 @@
+/* eslint-disable no-unused-vars */
+
 "use strict";
 
-/**
- * This is a dummy TypeScript test file using chai and mocha
- *
- * It's automatically excluded from npm and its build output is excluded from both git and npm.
- * It is advised to test all your modules with accompanying *.test.js-files
- */
-
-// tslint:disable:no-unused-expression
-
 const { should } = require("chai").should;
-const { getTypeOfGiraDatapoint } = require("../lib/homeserverTypes.js").getTypeOfGiraDatapoint;
+const { expect } = require("chai");
+const { getTypeOfGiraDatapoint } = require("../lib/homeserverTypes.js");
 
 describe("homeserverTypes => getTypeOfGiraDatapoint", () => {
 
-	it(`should return "Boolean on ID 1"`, () => {
+	it(`should return "boolean on ID 1`, () => {
 		const result = getTypeOfGiraDatapoint(1);
-		result.should.be.equal("Boolean");
+		result.should.be.equal("boolean");
 	});
-	// ... more tests => it
+	it(`Should return a String on ID 1`,() => {
+		const result = getTypeOfGiraDatapoint(1);
+		result.should.be.a("string");
+	});
+	it(`Should return a number on ID 2`,() => {
+		const result = getTypeOfGiraDatapoint(2);
+		result.should.be.equal("number");
+	});
+	it(`Should return a String on ID 2`,() => {
+		const result = getTypeOfGiraDatapoint(2);
+		result.should.be.a("string");
+	});
+	it(`Should return a number on ID 4`,() => {
+		const result = getTypeOfGiraDatapoint(4);
+		result.should.be.equal("number");
+	});
+	it(`Should return a String on ID 4`,() => {
+		const result = getTypeOfGiraDatapoint(4);
+		result.should.be.a("string");
+	});
+	it(`Should return a number on ID 9`,() => {
+		const result = getTypeOfGiraDatapoint(9);
+		result.should.be.equal("number");
+	});
+	it(`Should return a String on ID 9`,() => {
+		const result = getTypeOfGiraDatapoint(9);
+		result.should.be.a("string");
+	});
+	it(`Should return a number on ID 11`,() => {
+		const result = getTypeOfGiraDatapoint(11);
+		result.should.be.equal("number");
+	});
+	it(`Should return a String on ID 11`,() => {
+		const result = getTypeOfGiraDatapoint(11);
+		result.should.be.a("string");
+	});
+	it(`Should return a number on ID 12`,() => {
+		const result = getTypeOfGiraDatapoint(12);
+		result.should.be.equal("number");
+	});
+	it(`Should return a String on ID 12`,() => {
+		const result = getTypeOfGiraDatapoint(12);
+		result.should.be.a("string");
+	});
+	it(`Should return a number on ID 13`,() => {
+		const result = getTypeOfGiraDatapoint(13);
+		result.should.be.equal("number");
+	});
+	it(`Should return a String on ID 13`,() => {
+		const result = getTypeOfGiraDatapoint(13);
+		result.should.be.a("string");
+	});
+	it(`Should return a number on ID 14`,() => {
+		const result = getTypeOfGiraDatapoint(14);
+		result.should.be.equal("number");
+	});
+	it(`Should return a String on ID 14`,() => {
+		const result = getTypeOfGiraDatapoint(14);
+		result.should.be.a("string");
+	});
+	it(`Should return a number on ID 15`,() => {
+		const result = getTypeOfGiraDatapoint(15);
+		result.should.be.equal("number");
+	});
+	it(`Should return a String on ID 15`,() => {
+		const result = getTypeOfGiraDatapoint(15);
+		result.should.be.a("string");
+	});
+	it(`Should return text String on ID 22`,() => {
+		const result = getTypeOfGiraDatapoint(22);
+		result.should.be.equal("string");
+	});
+	it(`Should return a String on ID 22`,() => {
+		const result = getTypeOfGiraDatapoint(22);
+		result.should.be.a("string");
+	});
+	it(`Should return text Mixed on other ID`,() => {
+		const result = getTypeOfGiraDatapoint(3);
+		result.should.be.equal("Mixed");
+	});
+	it(`Should return a String on other ID`,() => {
+		const result = getTypeOfGiraDatapoint(3);
+		result.should.be.a("string");
+	});
+	it(`Should thrown an error on no parameter`,() => {
+		expect(getTypeOfGiraDatapoint).to.throw("no param");
+	});
+	it(`Should thrown an error on string-parameter`,() => {
+		expect(() => {getTypeOfGiraDatapoint("1");}).to.throw("parameter have to be of type number");
+	});
 });
 
-// ... more test suites => describe
